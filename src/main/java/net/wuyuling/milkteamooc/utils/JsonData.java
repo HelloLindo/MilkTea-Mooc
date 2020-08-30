5a269fe0b723ba0bc9a6276a96d640d8e41d3d91
lindowu <lindowu@foxmail.com>
2020-08-30 14:54:21 +0800

    Add JSON Wrapper.

---
+++ src/main/java/net/wuyuling/milkteamooc/utils/JsonData.java
@@ -0,0 +1,60 @@
+package net.wuyuling.milkteamooc.utils;
+
+public class JsonData {
+
+    private int code;
+
+    private Object data;
+
+    private String msg;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public JsonData() {
+    }
+
+    public JsonData(int code, Object data) {
+        this.code = code;
+        this.data = data;
+    }
+
+    public JsonData(int code, Object data, String msg) {
+        this.code = code;
+        this.data = data;
+        this.msg = msg;
+    }
+
+    public static JsonData buildSuccess(Object data) {
+        return new JsonData(0, data);
+    }
+
+    public static JsonData buildError(String msg) {
+        return new JsonData(-1, "", msg);
+    }
+
+    public static JsonData buildError(String msg, int code) {
+        return new JsonData(code, "", msg);
+    }
+}
